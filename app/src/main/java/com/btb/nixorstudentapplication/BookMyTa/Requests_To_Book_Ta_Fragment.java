d67ade7450e8b85ab17adc8fd8dcf1955fa38af8
Talha-Siddiqui1 <talha.siddiqui9560@gmail.com>
2018-06-20 03:22:00 +0500

    Started Working On Book_My_Ta

--- app/src/main/java/com/btb/nixorstudentapplication/BookMyTa/Requests_To_Book_Ta_Fragment.java
+++ app/src/main/java/com/btb/nixorstudentapplication/BookMyTa/Requests_To_Book_Ta_Fragment.java
@@ -4,9 +4,12 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.btb.nixorstudentapplication.R;
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -21,59 +24,47 @@ import com.google.firebase.firestore.QuerySnapshot;
 public class Requests_To_Book_Ta_Fragment extends Fragment {
 
     View view;
+    TextView statusText;
+
     public Requests_To_Book_Ta_Fragment() {
     }
-String TAG="Requests_To_Book_Ta_Fragment";
-String StudentName="";
+
+    String TAG = "Requests_To_Book_Ta_Fragment";
+    String StudentName = "";
+    String Status;
+
+    public void DisplayRequest() {
+        CollectionReference cr = FirebaseFirestore.getInstance().collection("BookMyTa");
+        Query NameQuery = cr.whereEqualTo("StudentName", "Talha");
+        cr.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (DocumentSnapshot document : task.getResult()) {
+                        Status = document.get("StudentName").toString();
+
+                    }
+                }
 
 
-public void DisplayRequest(){
-    CollectionReference cr = FirebaseFirestore.getInstance().collection("BookMyTa");
-    Query NameQuery=cr.whereEqualTo("StudentName","Talha");
-    cr.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-        @Override
-        public void onComplete(@NonNull Task<QuerySnapshot> task) {
-            if (task.isSuccessful()) {
-                for (DocumentSnapshot document : task.getResult()) {
-      
-
-
-
-
-
-
-
-
-        }
-    });
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-
-        view=inflater.inflate(R.layout.requests_to_book_ta,container,false);
-
-        return view;
+            }
+        });
 
     }
-}
+        @Nullable
+        @Override
+        public View onCreateView (LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState){
+
+            view = inflater.inflate(R.layout.requests_to_book_ta, container, false);
+           statusText=view.findViewById(R.id.booked_ta_request);
+
+        DisplayRequest();
+            //statusText.setText(Status);
+          Log.i(TAG,Status);
+            return view;
+
+        }
+    }
 
 
 
