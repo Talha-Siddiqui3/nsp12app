3adace3c735c732e99de1472908abc2759b0a684
Talha-Siddiqui1 <talha.siddiqui9560@gmail.com>
2018-06-21 17:18:07 +0500

    BOOKMYTA Progresss

--- app/src/main/java/com/btb/nixorstudentapplication/BookMyTa/RV_Adaptor_1.java
+++ app/src/main/java/com/btb/nixorstudentapplication/BookMyTa/RV_Adaptor_1.java
@@ -2,15 +2,20 @@ package com.btb.nixorstudentapplication.BookMyTa;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import com.btb.nixorstudentapplication.Past_papers.RvAdaptor;
+import com.btb.nixorstudentapplication.R;
 
 public class RV_Adaptor_1 extends RecyclerView.Adapter<RvAdaptor.Rv_ViewHolder> {
     @NonNull
     @Override
     public RvAdaptor.Rv_ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater inflater= LayoutInflater.from(parent.getContext());
+        View view=inflater.inflate(R.layout.student_requests_for_ta_rv,parent,false);
+        return new RvAdaptor.Rv_ViewHolder(view);
     }
 
     @Override
