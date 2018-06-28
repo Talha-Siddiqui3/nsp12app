b05c3a1323953593cc7cdfba75fe90da9f2b45a2
Talha-Siddiqui1 <talha.siddiqui9560@gmail.com>
2018-06-29 04:57:54 +0500

    Added Nav Bar and Header

--- app/src/main/java/com/btb/nixorstudentapplication/BookMyTa/Main_Activity_Ta_Tab.java
+++ app/src/main/java/com/btb/nixorstudentapplication/BookMyTa/Main_Activity_Ta_Tab.java
@@ -9,11 +9,13 @@ import com.btb.nixorstudentapplication.BookMyTa.Adaptors.ViewPageAdaptor;
 import com.btb.nixorstudentapplication.BookMyTa.Fragments_for_tabs.Requests_To_Book_Ta_Fragment;
 import com.btb.nixorstudentapplication.BookMyTa.Fragments_for_tabs.Search_Ta_Fragment;
 import com.btb.nixorstudentapplication.BookMyTa.Fragments_for_tabs.Student_Requests_For_Ta_Fragment;
+import com.btb.nixorstudentapplication.GeneralLayout.activity_header;
 import com.btb.nixorstudentapplication.R;
 
 public class Main_Activity_Ta_Tab extends AppCompatActivity {
     private TabLayout tablayout;
     private ViewPager viewPager;
+    private activity_header activity_header;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +23,14 @@ public class Main_Activity_Ta_Tab extends AppCompatActivity {
         setContentView(R.layout.activity_ta__tab);
         tablayout = findViewById(R.id.tablayout_id);
         viewPager = findViewById(R.id.viewpager_id);
+        activity_header=findViewById(R.id.toolbar_top_Ta_Tab);
         ViewPageAdaptor adaptor = new ViewPageAdaptor(getSupportFragmentManager());
         adaptor.AddFragment(new Search_Ta_Fragment(), "Search Ta");
         adaptor.AddFragment(new Student_Requests_For_Ta_Fragment(), "Requests for you");
         adaptor.AddFragment(new Requests_To_Book_Ta_Fragment(), "Your requests for Ta");
         viewPager.setAdapter(adaptor);
         tablayout.setupWithViewPager(viewPager);
+        activity_header.setActivityname("Bookmyta");
 
 
     }
