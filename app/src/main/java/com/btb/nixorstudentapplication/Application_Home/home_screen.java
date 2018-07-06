50b5a00c61622a0b7a99eaba13a3a0a846605bec
Hassan Abbasi <hassananjum331@gmail.com>
2018-07-07 00:45:47 +0500

    Added Navigation Drawer tho its design remains. Added a splash screen

--- app/src/main/java/com/btb/nixorstudentapplication/Application_Home/home_screen.java
+++ app/src/main/java/com/btb/nixorstudentapplication/Application_Home/home_screen.java
@@ -4,6 +4,7 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
@@ -12,11 +13,13 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.btb.nixorstudentapplication.BookMyTa.Main_Activity_Ta_Tab;
@@ -39,6 +42,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Timer;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -58,6 +62,7 @@ public class home_screen extends AppCompatActivity implements View.OnClickListen
     public static String Studentname;
     public static String StudentID;
     public static FirebaseFirestore db;
+    ImageView movingshark;
 
     //NSP
 
@@ -76,14 +81,15 @@ public class home_screen extends AppCompatActivity implements View.OnClickListen
         setContentView(R.layout.activity_home_screen);
 
 
-
         //XML
         photoStudent_circleView = findViewById(R.id.student_photo);
         idStudent_textView = findViewById(R.id.idStudent_textView);
         nameStudent_textView = findViewById(R.id.nameStudent_textView);
         mDrawerLayout = findViewById(R.id.drawer_layout);
         menu_button = findViewById(R.id.menu_button);
-
+        movingshark = findViewById(R.id.movingshark);
+        moveShark();
+        animateShark();
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         ActionBar actionbar = getSupportActionBar();
@@ -93,11 +99,17 @@ public class home_screen extends AppCompatActivity implements View.OnClickListen
                 new NavigationView.OnNavigationItemSelectedListener() {
                     @Override
                     public boolean onNavigationItemSelected(MenuItem menuItem) {
-                       mDrawerLayout.closeDrawers();
-                        switch (menuItem.getItemId()){
-                            case R.id.pp:startActivity(new Intent(home_screen.this, MainPPActivity.class));break;
-                            case R.id.soc:startActivity(new Intent(home_screen.this, MainCloudActivity.class));break;
-                            case R.id.ta:startActivity(new Intent(home_screen.this, Main_Activity_Ta_Tab.class));break;
+                        mDrawerLayout.closeDrawers();
+                        switch (menuItem.getItemId()) {
+                            case R.id.pp:
+                                startActivity(new Intent(home_screen.this, MainPPActivity.class));
+                                break;
+                            case R.id.soc:
+                                startActivity(new Intent(home_screen.this, MainCloudActivity.class));
+                                break;
+                            case R.id.ta:
+                                startActivity(new Intent(home_screen.this, Main_Activity_Ta_Tab.class));
+                                break;
                         }
                         return true;
                     }
@@ -108,16 +120,93 @@ public class home_screen extends AppCompatActivity implements View.OnClickListen
         common_util.checkActivation(home_screen.this, username);
         intializeNSP();
     }
+
     public void onClick(View view) {
-    switch (view.getId()) {
-        case R.id.menu_button:
-            mDrawerLayout.openDrawer(GravityCompat.START);
+        switch (view.getId()) {
+            case R.id.menu_button:
+                mDrawerLayout.openDrawer(GravityCompat.END);
+
+        }
+    }
+    static Boolean directionback = false;
+    static Boolean initialAnimation =true;
+    public void moveShark(){
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        final int width = displayMetrics.widthPixels-15;
+      int animTime = 0;
+        if(!initialAnimation){
+            animTime =15000;
+        }
+    new CountDownTimer(animTime, 1000) {
+
+        public void onTick(long millisUntilFinished) {
+
+        }
+
+        public void onFinish() {
+            initialAnimation=false;
+            directionback=false;
+            movingshark.animate().translationX(width).setDuration(15000);
+            new CountDownTimer(15000, 1000) {
+
+                public void onTick(long millisUntilFinished) {
+
+                }
+
+                public void onFinish() {
+                    directionback=true;
+                    movingshark.animate().translationX(0).setDuration(15000);
+                    moveShark();
+
+                }
+            }.start();
+        }
+    }.start();
+
+
+
+
+
+
+
+}
+    public void animateShark() {
+
+
+
+           new CountDownTimer(100, 1000) {
+               Boolean sharkset =true;
+               public void onTick(long millisUntilFinished) {
+
+               }
+
+               public void onFinish() {
+                   if(sharkset){
+                       if(directionback){
+                           movingshark.setImageResource(R.drawable.nixorsharkoriginal2back);
+                       }else{
+                       movingshark.setImageResource(R.drawable.nixorsharkoriginal2);}
+                       sharkset=true;
+                       animateShark();
+                   }else{
+                       if(directionback){
+                       movingshark.setImageResource(R.drawable.nixorsharkback);}
+                       else{
+                           movingshark.setImageResource(R.drawable.nixorsharkoriginal);
+                       }
+                       sharkset =false;
+                       animateShark();
+                   }
+               }
+           }.start();
+
+
 
     }
 
-    }
     //Nsp Stuff. Do your commenting here
-    public void intializeNSP(){
+    public void intializeNSP() {
         icons = new ArrayList<>();
         common_util = new common_util();
         GetGUIDLocally();
@@ -131,21 +220,25 @@ public class home_screen extends AppCompatActivity implements View.OnClickListen
 
 
     }
+
     public void MakePath() {
         File path = new File(Environment.getExternalStorageDirectory() + "/nixorapp/NspDocuments/");
         if (!path.exists())
             path.mkdirs();
 
     }
+
     public void GetGUIDLocally() {
         GUID = common_util.getUserDataLocally(this, "GUID");
 
     }
+
     private void GetExternalStoragePermission() {
         permission_util permission_util = new permission_util();
         String[] permissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE};
         permission_util.getPermissions(this, permissions);
     }
+
     public void GetNspIcons() {
         String username = common_util.getUserDataLocally(this, "username");
         DocumentReference dr = FirebaseFirestore.getInstance().collection("users").document(username).collection("icons").document("myicons");
@@ -232,13 +325,14 @@ public class home_screen extends AppCompatActivity implements View.OnClickListen
 
     @Override
     public void onBackPressed() {
-        if(mDrawerLayout!=null){
-            if(mDrawerLayout.isDrawerOpen(GravityCompat.START)){
+        if (mDrawerLayout != null) {
+            if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
                 mDrawerLayout.closeDrawers();
-            }else{
+            } else {
                 super.onBackPressed();
             }
-        }else{
-        super.onBackPressed();}
+        } else {
+            super.onBackPressed();
+        }
     }
 }
