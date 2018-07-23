7eed9d4c5f26b44bfeb283a0364c62d45e5c7634
Talha Siddiqui <talhasiddiqui922000@gmail.com>
2018-07-23 15:16:00 -0700

    initial commit

---
+++ app/src/main/java/com/btb/nixorstudentapplication/Carpool/Main_Activity_Carpool_Tab.java
@@ -0,0 +1,15 @@
+package com.btb.nixorstudentapplication.Carpool;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.btb.nixorstudentapplication.R;
+
+public class Main_Activity_Carpool_Tab extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_carpool_tab);
+    }
+}
